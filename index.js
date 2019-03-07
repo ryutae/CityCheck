c8141ebb9599dd078b87abef4d65fc70a89b6216
ryuta <ryuta.eguchi@gmail.com>
2019-03-06 19:16:45 -0800

    add styling to weather and foursquare

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+let destLoc = '';
+let markers = [];
 const fourURL = 'https://api.foursquare.com/v2/venues/explore?';
 const CLIENT_ID = 'KL5BWO4QF221XC3CFHVUQLRMKPOR3SE0USF5BOMVDI50F5RL';
 const CLIENT_SECRET = 'JQRUBFHSM1CGAANS1WXC3XFJ4NTUFITH3AN1UEYCVDHXGQBY';
@@ -9,6 +11,75 @@ const weatherURL = 'https://api.openweathermap.org/data/2.5/'
 const directionsURL = 'https://maps.googleapis.com/maps/api/directions/json?';
 const mapsAPI = 'AIzaSyDf4F76gQQxbu-fpuGgWklK72LdZoWF4Vg';
 
+var geocoder;
+
+function initialize() {
+  geocoder = new google.maps.Geocoder();
+  var latlng = new google.maps.LatLng(40.648610, -101.942230);
+  var mapOptions = {
+    zoom: 3,
+    center: latlng,
+    zoomControl: true
+  };
+  let map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+  // var marker = new google.maps.Marker({
+  //   // The below line is equivalent to writing:
+  //   // position: new google.maps.LatLng(-34.397, 150.644)
+  //   position: {
+  //     lat: -34.397,
+  //     lng: 150.644
+  //   },
+  //   map: map
+  // });
+  // var infowindow = new google.maps.InfoWindow({
+  //   content: '<p>Marker Location:' + marker.getPosition() + '</p>'
+  // });
+
+  // google.maps.event.addListener(marker, 'click', function() {
+  //   infowindow.open(map, marker);
+  // });
+}
+// google.maps.event.addDomListener(window, 'load', initialize);
+
+function cleanMarkers() {
+  markers = [];
+}
+//
+// function centerMapToLocation(loc) {
+//   var addressSearch = loc;
+//
+//   getLatLngFromAddressAsync(addressSearch, function(location) {
+//     map.setCenter(location);
+//     map.setZoom(7);
+//   });
+// }
+
+function getLatLngFromAddressAsync(addressSearch, callback) {
+
+  if (!addressSearch) {
+    return;
+  }
+  geocoder.geocode({
+    'address': addressSearch
+  }, function(results, status) {
+    if (status == 'OK') {
+      callback(results[0].geometry.location)
+    }
+  });
+}
+/*
+function createMarker(latlon, currentPetName, index){
+
+	var marker = new google.maps.Marker({
+		position: latlon,
+		map: map,
+		title: currentPetName,
+		icon: 'images/icn_blue.png'
+	});
+	state.markers[index]=marker
+}*/
+
 function formatQueryParams(params) {
   const queryItems = Object.keys(params).map(key =>
     `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
@@ -18,20 +89,21 @@ function formatQueryParams(params) {
 
 function renderFoursquarePillCategories(elements) {
   let result = '';
-  for (let i=0; i<elements.length; i++) {
+  for (let i = 0; i < elements.length; i++) {
     result += `<div>${elements[i].name}</div>`
   };
   return result;
 }
 
 function renderFoursquarePill(element, index) {
-  return   `<div class="foursquare-page">
-      <p class="pill-title"><span>${index+1}.</span> ${element.venue.name} </p>
-      <p class="pill-description">${element.venue.categories[0].name}</p>
+  return `<div class="foursquare-page">
+      <p class="pill-title"><span>${index+1}.</span> <a href="https://foursquare.com/v/${element.venue.id}" target="_blank">${element.venue.name}</a>       <button class="foursquare-pill-button">Like</button>
+            <button class="foursquare-pill-button">Dislike</button></p>
       <div class="pill-categories">` +
-      renderFoursquarePillCategories(element.venue.categories) +
+    renderFoursquarePillCategories(element.venue.categories) +
 
-      `</div>
+    `</div>
+
       <ul>
         <li>${element.venue.location.address}
         ${element.venue.location.city}, ${element.venue.location.state}
@@ -41,6 +113,7 @@ function renderFoursquarePill(element, index) {
         </div>
     `
 }
+let foursquareResponse = [];
 
 function getFoursquare(fourParams) {
   console.log('foursquare response:');
@@ -49,6 +122,7 @@ function getFoursquare(fourParams) {
     .then(response => response.json())
     .then(responseJson => {
       console.log(responseJson);
+      foursquareResponse = responseJson;
       $('.foursquare').append(`<h2>Foursquare Recommended places at ${responseJson.response.geocode.displayString}</h2>`);
       for (let i = 0; i < responseJson.response.groups[0].items.length; i++) {
         $('.foursquare').append(renderFoursquarePill(responseJson.response.groups[0].items[i], i)
@@ -68,13 +142,27 @@ function getWeatherForecast(weatherParams) {
     .then(response => response.json())
     .then(responseJson => {
       console.log(responseJson);
-      $('.weather').append(
-        `<h2>Weather Forecast</h2>
 
-        <pre>${JSON.stringify(responseJson, null, 4)}</pre>`
-      );
+      $('.weather').append(
+        `<p class="section-header">Weather Forecast</p>`);
+      for (let i = 0; i < responseJson.list.length; i++) {
+        debugger;
+        $('.weather').append(renderWeatherForecast(responseJson.list[i]))
+      };
+      //$('.weather').append(`<pre>${JSON.stringify(responseJson, null, 4)}</pre>`)
     })
     .catch();
+}
+
+function renderWeatherForecast(element) {
+  let result = '';
+  debugger;
+  result += `<div class="weather-forecast-pill weather-container">
+        <p class="weather-date">${element.dt_txt}</p>
+        <img class="weather-icon" src="http://openweathermap.org/img/w/${element.weather[0].icon}.png">
+        <p class="weather-icon-details">${element.weather[0].main} - ${element.weather[0].description}</p>
+        <p class="weather-details">Temp: ${Math.round(element.main.temp)} Humidity: ${element.main.humidity} </p>      </div>`;
+  return result;
 }
 
 function getWeatherCurrent(weatherParams) {
@@ -85,26 +173,34 @@ function getWeatherCurrent(weatherParams) {
     .then(responseJson => {
       console.log(responseJson);
       $('.weather').append(
-        `<h2>Weather Current</h2>
-                <p><img src="http://openweathermap.org/img/w/${responseJson.weather[0].icon}.png"> ${responseJson.weather[0].main} - ${responseJson.weather[0].description}</p>
-                <p>Current temp: ${responseJson.main.temp}  Current humidity: ${responseJson.main.humidity}
-        <pre>${JSON.stringify(responseJson, null, 4)}</pre>`
-      );
+        `<p class="section-header">Weather Current</p>
+        <div class="weather-current-container">
+                <div class="weather-container"> <img class="weather-icon" src="http://openweathermap.org/img/w/${responseJson.weather[0].icon}.png"> <p class="weather-icon-details">${responseJson.weather[0].main} - ${responseJson.weather[0].description}</p>
+                <p class="weather-details">Current temp: ${Math.round(responseJson.main.temp)}</>  <p class="weather-details">Current humidity: ${responseJson.main.humidity}</p>
+        </div>`);
+        // $('.weather').append(`
+        //   Weather Current Response:
+        // <pre>${JSON.stringify(responseJson, null, 4)}</pre>
+        // `
+      // );
     })
     .catch();
 }
 
-let map;
 
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {
-      lat: -34.397,
-      lng: 150.644
-    },
-    zoom: 8
-  });
-}
+
+
+// let map;
+//
+// function initMap() {
+//   map = new google.maps.Map(document.getElementById('map'), {
+//     center: {
+//       lat: -34.397,
+//       lng: 150.644
+//     },
+//     zoom: 8
+//   });
+// }
 
 /*
 var xhr = new XMLHttpRequest();
@@ -156,12 +252,12 @@ function watchSubmit() {
     e.preventDefault();
     $('.weather').empty();
     $('.foursquare').empty();
-    $('.maps').empty();
+    // $('.maps').empty();
     $('.error-msg').empty();
     console.log($('input[name="current-loc"]').val());
     console.log($('input[name="destination-loc"]').val());
     let startLoc = $('input[name="current-loc"]').val();
-    let destLoc = 'goleta';//$('input[name="destination-loc"]').val();
+    destLoc = 'goleta'; //$('input[name="destination-loc"]').val();
     const fourParams = {
       client_id: CLIENT_ID,
       client_secret: CLIENT_SECRET,
@@ -175,20 +271,23 @@ function watchSubmit() {
       units: 'imperial', //imperial-f, metric-c, standard-k
       APPID: weatherAPI
     };
-    const directionsParams = {
-      origin: startLoc,
-      destination: destLoc,
-      key: mapsAPI
-    };
-    const distanceMatrixParams = {
-      origins: startLoc,
-      destinations: destLoc,
-      key: mapsAPI,
-      units: 'imperial'
-    };
+    // const directionsParams = {
+    //   origin: startLoc,
+    //   destination: destLoc,
+    //   key: mapsAPI
+    // };
+    // const distanceMatrixParams = {
+    //   origins: startLoc,
+    //   destinations: destLoc,
+    //   key: mapsAPI,
+    //   units: 'imperial'
+    // };
     getWeatherCurrent(weatherParams);
     getWeatherForecast(weatherParams);
     getFoursquare(fourParams);
+    // cleanMarkers();
+    initialize();
+    // centerMapToLocation(destLoc);
     //  getDistanceMatrix(distanceMatrixParams);
     // let fourRequest = fourURL + formatQueryParams(fourParams);
     // // let fourRequest = `https://api.foursquare.com/v2/venues/search?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&query=coffee&near=Goleta&v=20190228`
