707399b4846da2edac38c9ca3edb88141983507d
ryuta <ryuta.eguchi@gmail.com>
2019-03-08 17:10:16 -0800

    Add css to buttons

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ const fourURL = 'https://api.foursquare.com/v2/venues/explore?';
 
 const weatherURL = 'https://api.openweathermap.org/data/2.5/'
 const directionsURL = 'https://maps.googleapis.com/maps/api/directions/json?';
-var geocoder;
-
+//geocoder = new google.maps.Geocoder();
 function initialize() {
   geocoder = new google.maps.Geocoder();
   var latlng = new google.maps.LatLng(40.648610, -101.942230);
@@ -93,8 +92,8 @@ function renderFoursquarePillCategories(elements) {
 
 function renderFoursquarePill(element, index) {
   return `<div class="foursquare-page">
-      <p class="pill-title"><span>${index+1}.</span> <a href="https://foursquare.com/v/${element.venue.id}" target="_blank">${element.venue.name}</a>       <button class="foursquare-pill-button">Like</button>
-            <button class="foursquare-pill-button">Dislike</button></p>
+      <p class="pill-title"><span>${index+1}.</span> <a href="https://foursquare.com/v/${element.venue.id}" target="_blank">${element.venue.name}</a>       <button class="foursquare-pill-button foursquare-pill-button-like">Check Out!</button>
+            <button class="foursquare-pill-button foursquare-pill-button-dislike">Dislike</button></p>
       <div class="pill-categories">` +
     renderFoursquarePillCategories(element.venue.categories) +
 
@@ -119,7 +118,7 @@ function getFoursquare(fourParams) {
     .then(responseJson => {
       console.log(responseJson);
       foursquareResponse = responseJson;
-      $('.foursquare').append(`<h2>Foursquare Recommended places at ${responseJson.response.geocode.displayString}</h2>`);
+      $('.foursquare').append(`<p class="section-header">Foursquare Recommended places at ${responseJson.response.geocode.displayString}</p>`);
       for (let i = 0; i < responseJson.response.groups[0].items.length; i++) {
         $('.foursquare').append(renderFoursquarePill(responseJson.response.groups[0].items[i], i)
 
@@ -128,8 +127,11 @@ function getFoursquare(fourParams) {
       $('.foursquare').append(
         `<pre>${JSON.stringify(responseJson, null, 4)}</pre>`
       );
-    })
+    });
 }
+
+
+let forecastResponse = [];
 
 function getWeatherForecast(weatherParams) {
   console.log('weather forecast response:');
@@ -138,10 +140,10 @@ function getWeatherForecast(weatherParams) {
     .then(response => response.json())
     .then(responseJson => {
       console.log(responseJson);
-
+      forecastResponse = responseJson;
       $('.weather').append(
         `<p class="section-header">Weather Forecast</p>`);
-      for (let i = 0; i < responseJson.list.length; i++) {
+      for (let i = 0; i < responseJson.list.length; i += 8) {
         debugger;
         $('.weather').append(renderWeatherForecast(responseJson.list[i]))
       };
@@ -150,10 +152,32 @@ function getWeatherForecast(weatherParams) {
     .catch();
 }
 
+const dayOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
 function renderWeatherForecast(element) {
   let result = '';
+  let dateForecast = new Date(element.dt * 1000);
+  const dateObject = {
+    month: month[dateForecast.getMonth()],
+    date: dateForecast.getDate(),
+    day: dayOfWeek[dateForecast.getDay()],
+    time: dateForecast.getTime()
+  };
+  let dateForecastReadable = dateForecast.toDateString();
+  //let month = dateForecast.getMonth();
+  let dateTimeForecast = dateForecast.toLocaleTimeString(undefined, {
+    month: 'short',
+    day: 'numeric',
+    weekday: 'short',
+    hour: '2-digit',
+    minute: '2-digit'
+  });
+  //let dateString = `${dateObject.day} ${dateObject.month} ${dateObject.date}  ${time}`;
+  //add to a forecastArray days starting from tomorrow
+
   result += `<div class="weather-forecast-pill weather-container">
-        <p class="weather-date">${element.dt_txt}</p>
+        <p class="weather-date">${dateTimeForecast}</p>
         <img class="weather-icon" src="http://openweathermap.org/img/w/${element.weather[0].icon}.png">
         <p class="weather-icon-details">${element.weather[0].main} - ${element.weather[0].description}</p>
         <p class="weather-details">Temp: ${Math.round(element.main.temp)} Humidity: ${element.main.humidity} </p>      </div>`;
@@ -168,15 +192,14 @@ function getWeatherCurrent(weatherParams) {
     .then(responseJson => {
       console.log(responseJson);
       $('.weather').append(
-        `<p class="section-header">Weather Current</p>
-        <div class="weather-current-container">
+        `<p class="section-header">Current Weather at ${responseJson.name}</p>
                 <div class="weather-container"> <img class="weather-icon" src="http://openweathermap.org/img/w/${responseJson.weather[0].icon}.png"> <p class="weather-icon-details">${responseJson.weather[0].main} - ${responseJson.weather[0].description}</p>
                 <p class="weather-details">Current temp: ${Math.round(responseJson.main.temp)}</>  <p class="weather-details">Current humidity: ${responseJson.main.humidity}</p>
         </div>`);
-        // $('.weather').append(`
-        //   Weather Current Response:
-        // <pre>${JSON.stringify(responseJson, null, 4)}</pre>
-        // `
+      // $('.weather').append(`
+      //   Weather Current Response:
+      // <pre>${JSON.stringify(responseJson, null, 4)}</pre>
+      // `
       // );
     })
     .catch();
@@ -252,7 +275,9 @@ function watchSubmit() {
     console.log($('input[name="current-loc"]').val());
     console.log($('input[name="destination-loc"]').val());
     let startLoc = $('input[name="current-loc"]').val();
-    destLoc = 'goleta'; //$('input[name="destination-loc"]').val();
+    //destLoc = 'goleta'; //$('input[name="destination-loc"]').val();
+    destLoc = $('input[name="destination-loc"]').val() ? $('input[name="destination-loc"]').val() : 'goleta';
+
     const fourParams = {
       client_id: CLIENT_ID,
       client_secret: CLIENT_SECRET,
@@ -298,8 +323,20 @@ function watchSubmit() {
   })
 }
 
+function watchButtonLike() {
+  $("body").on("click", ".foursquare-pill-button-like", function(e) {
+    e.preventDefault;
+    $(this).toggleClass("like-button-selected");
+  });
+  $("body").on("click", ".foursquare-pill-button-dislike", function(e) {
+    e.preventDefault;
+    $(this).toggleClass("dislike-button-selected");
+  })
+}
+
 $(function() {
   console.log('app loaded');
   watchSubmit();
   watchCurrentLocation();
+  watchButtonLike();
 });
