2709c0836aa1aa2bd8ce32aaa892941e413fa49d
ryuta <ryuta.eguchi@gmail.com>
2019-03-11 18:38:49 -0700

    Adjust font-sizes

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ function formatQueryParams(params) {
   return queryItems.join('&');
 }
 
+let weatherCurrentResponse = {};
+
 function getWeatherCurrent(weatherParams) {
   console.log('weather current response:');
   let weatherCurrentRequest = weatherURL + 'weather?' + formatQueryParams(weatherParams);
@@ -21,11 +23,8 @@ function getWeatherCurrent(weatherParams) {
     .then(response => response.json())
     .then(responseJson => {
       console.log(responseJson);
-      $('.weather-current').append(
-        `<p class="section-header">${responseJson.name} Current Weather</p>
-                <div class="weather-container"> <img class="weather-icon" src="http://openweathermap.org/img/w/${responseJson.weather[0].icon}.png"> <p class="weather-icon-details">${responseJson.weather[0].main}:  ${responseJson.weather[0].description}</p>
-                <p class="weather-details">${Math.round(responseJson.main.temp)}°F,     ${responseJson.main.humidity}% humidity</p>
-        </div>`);
+      weatherCurrentResponse = responseJson;
+      renderCurrentWeather(responseJson);
       // $('.weather').append(`
       //   Weather Current Response:
       // <pre>${JSON.stringify(responseJson, null, 4)}</pre>
@@ -33,6 +32,18 @@ function getWeatherCurrent(weatherParams) {
       // );
     })
     .catch();
+}
+
+function renderCurrentWeather(responseJson) {
+  if (responseJson.cod != 200) {
+    $('.error-msg').append(`Error getting weather: ${responseJson.message} </br>`)
+  } else {
+    $('.weather-current').append(
+      `<p class="section-header">${responseJson.name} Current Weather</p>
+            <div class="weather-container"> <img class="weather-icon" src="http://openweathermap.org/img/w/${responseJson.weather[0].icon}.png"> <p class="weather-icon-details">${responseJson.weather[0].main}:  ${responseJson.weather[0].description}</p>
+            <p class="weather-details">${Math.round(responseJson.main.temp)}°F,     ${responseJson.main.humidity}% humidity</p>
+    </div>`);
+  }
 }
 
 let forecastResponse = [];
@@ -45,11 +56,13 @@ function getWeatherForecast(weatherParams) {
     .then(responseJson => {
       console.log(responseJson);
       forecastResponse = responseJson;
-      $('.weather-forecast').append(
-        `<p class="section-header">Weather Forecast</p>`);
-      for (let i = 0; i < responseJson.list.length; i += 8) {
-        $('.weather-forecast').append(renderWeatherForecast(responseJson.list[i]))
-      };
+      if (responseJson.cod == 200) {
+        $('.weather-forecast').append(
+          `<p class="section-header">Weather Forecast</p>`);
+        for (let i = 0; i < responseJson.list.length; i += 8) {
+          $('.weather-forecast').append(renderWeatherForecast(responseJson.list[i]))
+        };
+      }
       //$('.weather').append(`<pre>${JSON.stringify(responseJson, null, 4)}</pre>`)
     })
     .catch();
@@ -81,7 +94,7 @@ function renderWeatherForecast(element) {
   result += `<div class="weather-container">
         <p class="weather-date">${dateString}</p>
         <img class="weather-icon" src="http://openweathermap.org/img/w/${element.weather[0].icon}.png">
-        <p class="weather-icon-details">${element.weather[0].main} - ${element.weather[0].description}</p>
+        <p class="weather-icon-details">${element.weather[0].main}:  ${element.weather[0].description}</p>
         <p class="weather-details">${Math.round(element.main.temp)}°F, ${element.main.humidity}% humidity</p>      </div>`;
   return result;
 }
@@ -104,8 +117,8 @@ function renderFoursquarePill(element, index) {
     address = 'Address not found'
   };
   return `<div class="foursquare-page">
-      <p class="pill-title"><span>${index+1}.</span> <a href="https://foursquare.com/v/${element.venue.id}" target="_blank">${element.venue.name}</a>       <button class="foursquare-pill-button foursquare-pill-button-like">Check Out!</button>
-            <button class="foursquare-pill-button foursquare-pill-button-dislike">Dislike</button></p>
+      <p class="pill-title"><span>${index+1}.</span> <a href="https://foursquare.com/v/${element.venue.id}" target="_blank">${element.venue.name}</a> </p>      <button class="foursquare-pill-button foursquare-pill-button-like">Check Out!</button>
+            <button class="foursquare-pill-button foursquare-pill-button-dislike">Dislike</button>
       <div class="pill-categories">` +
     renderFoursquarePillCategories(element.venue.categories) +
 
@@ -126,78 +139,81 @@ function getFoursquare(fourParams) {
   fetch(fourRequest)
     .then(response => response.json())
     .then(responseJson => {
-      console.log(responseJson);
-      foursquareResponse = responseJson;
-      $('.foursquare').append(`<p class="section-header">Foursquare Recommended places at ${responseJson.response.geocode.displayString}</p>`);
-      for (let i = 0; i < responseJson.response.groups[0].items.length; i++) {
-        $('.foursquare').append(renderFoursquarePill(responseJson.response.groups[0].items[i], i)
+        console.log(responseJson);
+        foursquareResponse = responseJson;
+        if (responseJson.meta.code != 200) {
+          $('.error-msg').append(`Error getting recommended places: ${responseJson.meta.errorDetail}`)
+          }
+          else {
+            $('.foursquare').append(`<p class="section-header">Recommended places at ${responseJson.response.geocode.displayString}</p>`);
+            for (let i = 0; i < responseJson.response.groups[0].items.length; i++) {
+              $('.foursquare').append(renderFoursquarePill(responseJson.response.groups[0].items[i], i))
+            };
+          };
+          //  $('.foursquare').append(
+          //     `<pre>${JSON.stringify(responseJson, null, 4)}</pre>`
+          //   );
+        });
+    }
 
-        )
+  // function watchCurrentLocation() {
+  //   console.log('use current loc');
+  //   $('#use-current-loc').click(e => {
+  //     e.preventDefault();
+  //     getLocation();
+  //   })
+  // }
+
+  function watchSubmit() {
+    $('.button-start-search').click(e => {
+      e.preventDefault();
+      $(".set-cities").detach().prependTo(".results");
+      $('.start-screen').addClass('hidden');
+      $('.weather-current').empty();
+      $('.weather-forecast').empty();
+      $('.foursquare').empty();
+      // $('.maps').empty();
+      $('.error-msg').empty();
+      console.log($('input[name="current-loc"]').val());
+      console.log($('input[name="destination-loc"]').val());
+      let startLoc = $('input[name="current-loc"]').val();
+      //destLoc = 'goleta'; //$('input[name="destination-loc"]').val();
+      destLoc = $('input[name="destination-loc"]').val() ? $('input[name="destination-loc"]').val() : 'goleta';
+
+      const fourParams = {
+        client_id: CLIENT_ID,
+        client_secret: CLIENT_SECRET,
+        near: destLoc,
+        section: 'topPicks',
+        //query: 'things to do',
+        v: '20190228'
       };
-      //  $('.foursquare').append(
-      //     `<pre>${JSON.stringify(responseJson, null, 4)}</pre>`
-      //   );
+      const weatherParams = {
+        q: destLoc,
+        units: 'imperial', //imperial-f, metric-c, standard-k
+        APPID: weatherAPI
+      };
+
+      getWeatherCurrent(weatherParams);
+      getWeatherForecast(weatherParams);
+      getFoursquare(fourParams);
+
+    })
+  }
+
+  function watchButtonLike() {
+    $("body").on("click", ".foursquare-pill-button-like", function(e) {
+      e.preventDefault;
+      $(this).toggleClass("like-button-selected");
     });
-}
+    $("body").on("click", ".foursquare-pill-button-dislike", function(e) {
+      e.preventDefault;
+      $(this).toggleClass("dislike-button-selected");
+    })
+  }
 
-// function watchCurrentLocation() {
-//   console.log('use current loc');
-//   $('#use-current-loc').click(e => {
-//     e.preventDefault();
-//     getLocation();
-//   })
-// }
-
-function watchSubmit() {
-  $('.button-start-search').click(e => {
-    e.preventDefault();
-    $(".set-cities").detach().prependTo(".results");
-    $('.start-screen').addClass('hidden');
-    $('.weather-current').empty();
-    $('.weather-forecast').empty();
-    $('.foursquare').empty();
-    // $('.maps').empty();
-    $('.error-msg').empty();
-    console.log($('input[name="current-loc"]').val());
-    console.log($('input[name="destination-loc"]').val());
-    let startLoc = $('input[name="current-loc"]').val();
-    //destLoc = 'goleta'; //$('input[name="destination-loc"]').val();
-    destLoc = $('input[name="destination-loc"]').val() ? $('input[name="destination-loc"]').val() : 'goleta';
-
-    const fourParams = {
-      client_id: CLIENT_ID,
-      client_secret: CLIENT_SECRET,
-      near: destLoc,
-      section: 'topPicks',
-      //query: 'things to do',
-      v: '20190228'
-    };
-    const weatherParams = {
-      q: destLoc,
-      units: 'imperial', //imperial-f, metric-c, standard-k
-      APPID: weatherAPI
-    };
-
-    getWeatherCurrent(weatherParams);
-    getWeatherForecast(weatherParams);
-    getFoursquare(fourParams);
-
-  })
-}
-
-function watchButtonLike() {
-  $("body").on("click", ".foursquare-pill-button-like", function(e) {
-    e.preventDefault;
-    $(this).toggleClass("like-button-selected");
+  $(function() {
+    console.log('app loaded');
+    watchSubmit();
+    watchButtonLike();
   });
-  $("body").on("click", ".foursquare-pill-button-dislike", function(e) {
-    e.preventDefault;
-    $(this).toggleClass("dislike-button-selected");
-  })
-}
-
-$(function() {
-  console.log('app loaded');
-  watchSubmit();
-  watchButtonLike();
-});
